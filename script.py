d9917020489c7e798ac4580fbeb15ce0b516a2e0
Frank <frankdewijk@gmail.com>
2024-10-03 13:08:10 +0200

    fix

--- script.py
+++ script.py
@@ -1,14 +1,16 @@
-import os
+import sys
 
-# get the input and convert it to int
-num = os.environ.get("INPUT_NUM")
-if num:
+
+def square_number(n):
+    return n * n
+
+
+if __name__ == "__main__":
     try:
-        num = int(num)
-    except Exception:
-        exit('ERROR: the INPUT_NUM provided ("{}") is not an integer'.format(num))
-else:
-    num = 1
-
-# to set output, print to shell in following syntax
-print(f"::set-output name=num_squared::{num ** 2}")
+        input_number = int(sys.argv[1])
+        result = square_number(input_number)
+        print(f"::set-output name=result::{result}")
+        print(f"The square of {input_number} is {result}")
+    except (IndexError, ValueError):
+        print("Error: Please provide a valid integer as input.")
+        sys.exit(1)
